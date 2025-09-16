c884f24dd08a3e7e8c7d4a03f13078cda88e380a
AndrewL789 <Andrewl434@nycstudents.net>
2025-09-16 09:24:32 -0400

    f strings

---
+++ data.py
@@ -0,0 +1,56 @@
+# floats r for numbers with decimal values 
+
+""" x = 3 
+y = float(3)
+print(x,y)
+
+
+values = [1,2.23,5,7,2,30,15]
+print(values)
+for i in values:
+    print(i)
+
+
+print(values[0])
+print(values[6])
+
+ """
+
+# strings r a list of letters  to make text
+
+""" x = "this is a thing"
+y = x.split()
+z= y[0]
+print(y)
+print(z)
+
+ """
+
+""" #challenge 
+blah = input("say something")
+z = blah.split()
+y =(len(z))
+#len is for the length of the list "a"
+print(y)
+
+ """
+
+
+#booleans
+
+""" dow = input("what day is it")
+if dow == "Friday":
+    print ("correct")
+else:
+    print("incorrect")
+ """
+# f strings used b4 a string 
+
+""" x = "test"
+print (f"hello {x}") """
+
+
+temp = 75
+if temp > 68:
+    print('warm')
+
